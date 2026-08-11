46f89870d19ec2137a0af665bc9a7c9b8ffdb8c3
NgDuyPhong <nguyenduyphong997@gmail.com>
2026-08-11 21:56:37 +0700

    Update CSRF token validation to allow all routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,8 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
         $middleware->validateCsrfTokens(except: [
-            'api/*',
-            'webhook/*',
+            '*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
